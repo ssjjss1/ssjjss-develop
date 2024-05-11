bcaa9956d3aaff3c091bbf2314c62cece402e1d1
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2024-05-11 23:06:35 +0900

    [Gold I] Title: 구두 수선공, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- C++17/백준/Gold/14908. 구두 수선공/구두 수선공.cc
+++ C++17/백준/Gold/14908. 구두 수선공/구두 수선공.cc
@@ -1,26 +1,11 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
-int n;
 struct P{float f;int a,b,c;};
-bool compare(const P &x,const P &y){
-  if(x.f==y.f){
-    return x.c<y.c;
-  }
-  return x.f>y.f;
-}
+bool compare(const P &x,const P &y){return x.f==y.f? x.c<y.c:x.f>y.f;}
 vector<P> v;
 int main(){
-  ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-  cin>>n;
-  for(int i=1;i<=n;i++){
-    int x,y;
-    cin>>x>>y;
-    v.push_back({float(y)/float(x),x,y,i});
-  }
+  int n;cin>>n;
+  for(int i=1,x,y;i<=n;i++)cin>>x>>y,v.push_back({float(y)/float(x),x,y,i});
   sort(v.begin(),v.end(),compare);
-  for(int i=0;i<n;i++){
-    cout<<v[i].c<<" ";
-  }
+  for(int i=0;i<n;i++)cout<<v[i].c<<" ";
 }
