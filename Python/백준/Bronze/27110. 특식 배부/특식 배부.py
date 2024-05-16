d4cd823d7bb7cb9b38cd7aa56ac752744056cdae
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2024-05-16 22:42:38 +0900

    [Bronze IV] Title: 특식 배부, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/27110. 특식 배부/특식 배부.py
@@ -0,0 +1,9 @@
+N = int(input())
+chicken = list(map(int, input().split()))
+result = 0
+for i in range(3) :
+    if chicken[i] <= N :
+        result += chicken[i]
+    else :
+        result += N
+print(result)
