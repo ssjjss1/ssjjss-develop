ee87daacd0823d0d5213f92a86c7eb801130c02e
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2024-06-17 11:53:46 +0900

    [Gold V] Title: 거의 소수, Time: 120 ms, Memory: 9396 KB -BaekjoonHub

--- C++17/백준/Gold/1456. 거의 소수/거의 소수.cc
+++ C++17/백준/Gold/1456. 거의 소수/거의 소수.cc
@@ -1,13 +1,9 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 ll n, m, cnt;
 vector<int> v1;
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
   vector<bool> v(1e7 + 4, 0);
   for (int i = 2; i <= 1e7 + 1; i++) {
     if (!v[i]) {
@@ -21,8 +17,7 @@ int main() {
   for (auto i : v1) {
     for (ll j = i; j <= m / i;) {
       j*=i;
-      if (j >= n)
-        cnt += 1;
+      if (j >= n) cnt += 1;
     }
   }
   cout << cnt;
