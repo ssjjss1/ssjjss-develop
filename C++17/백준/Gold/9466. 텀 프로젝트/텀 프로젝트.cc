1b76c08f19ec7bf36bcda76a2a53a52a533a0f74
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2024-06-18 23:33:13 +0900

    [Gold III] Title: 텀 프로젝트, Time: 588 ms, Memory: 4344 KB -BaekjoonHub

--- C++17/백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
+++ C++17/백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
@@ -1,66 +1,56 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
 int t, n;
 vector<int> v;
 vector<int> visited;
 vector<int> finished;
 int cnt;
-
-void dfs(int start) {
-    int current = start;
-    vector<int> stack;
-    while (true) {
-        stack.push_back(current);
-        visited[current] = 1;
-        int next = v[current];
-
-        if (visited[next]) {
-            if (!finished[next]) {
-                // 사이클을 찾았을 때 사이클 길이를 카운트
-                while (!stack.empty()) {
-                    int top = stack.back();
-                    stack.pop_back();
-                    cnt++;
-                    finished[top] = 1;
-                    if (top == next) break;
-                }
-            }
-            break;
+void f(int x) {
+  int k = x;
+  vector<int> _v;
+  while (true) {
+    _v.push_back(k);
+    visited[k] = 1;
+    int next_k = v[k];
+    if (visited[next_k]) {
+      if (!finished[next_k]) {
+        while (!_v.empty()) {
+          int xx = _v.back();
+          _v.pop_back();
+          cnt += 1;
+          finished[xx] = 1;
+          if (xx == next_k) break;
         }
-
-        current = next;
+      }
+      break;
     }
-
-    while (!stack.empty()) {
-        int top = stack.back();
-        stack.pop_back();
-        finished[top] = 1;
-    }
+    k = next_k;
+  }
+  while (!_v.empty()) {
+    int y = _v.back();
+    _v.pop_back();
+    finished[y] = 1;
+  }
 }
-
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    cin >> t;
-    while (t--) {
-        cin >> n;
-        v.resize(n + 1);
-        visited.assign(n + 1, 0);
-        finished.assign(n + 1, 0);
-        cnt = 0;
-
-        for (int i = 1; i <= n; i++) {
-            cin >> v[i];
-        }
-
-        for (int i = 1; i <= n; i++) {
-            if (!visited[i]) {
-                dfs(i);
-            }
-        }
-
-        cout << n - cnt << "\n";
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  cin >> t;
+  while (t--) {
+    cin >> n;
+    v.resize(n + 4);
+    visited.assign(n + 4, 0);
+    finished.assign(n + 4, 0);
+    cnt = 0;
+    for (int i = 1; i <= n; i++)
+      cin >> v[i];
+    for (int i = 1; i <= n; i++) {
+      if (!visited[i]) {
+        f(i);
+      }
     }
-    return 0;
+    cout << n - cnt << "\n";
+  }
 }
