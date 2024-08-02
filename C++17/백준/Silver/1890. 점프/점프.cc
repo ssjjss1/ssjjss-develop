9dc931d74ea24947d7628c70e9024c6d50a2b25b
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2024-08-02 11:58:21 +0900

    [Silver I] Title: 점프, Time: 0 ms, Memory: 2148 KB -BaekjoonHub

--- C++17/백준/Silver/1890. 점프/점프.cc
+++ C++17/백준/Silver/1890. 점프/점프.cc
@@ -3,7 +3,6 @@ using namespace std;
 int n, d[104][104];
 long long dp[104][104];
 int main() {
-    //ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> n;
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
