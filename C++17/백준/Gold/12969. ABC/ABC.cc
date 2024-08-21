9ce31654fa558ffe111ad65d6952cbfaa9a8d87d
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2024-08-21 12:37:04 +0900

    [Gold I] Title: ABC, Time: 160 ms, Memory: 60672 KB -BaekjoonHub

---
+++ C++17/백준/Gold/12969. ABC/ABC.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int n, k;
+int dp[31][31][31][504]; 
+bool f(int x, int y, int z, int sum, string s) {
+  if (x + y + z == n) {
+    if (sum == k&&x>0&&y>0&&z>0) {
+      cout << s;
+      return 1;
+    }
+    return 0;
+  }
+  if (dp[x][y][z][sum] != -1)  return 0;
+  dp[x][y][z][sum] = 0;
+  if (f(x + 1, y, z, sum, s + 'A')) return 1;
+  if (f(x, y + 1, z, sum + x, s + 'B')) return 1;
+  if (f(x, y, z + 1, sum + x + y, s + 'C')) return 1;
+  return 0;
+}
+
+int main() {
+  ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+  cin >> n >> k;
+  for (int i = 0; i <= n; i++) {
+    for (int j = 0; j <= n; j++) {
+      for (int l = 0; l <= n; l++) {
+        for (int m = 0; m <= 503; m++) {
+          dp[i][j][l][m] = -1;
+        }
+      }
+    }
+  }
+  if (!f(0, 0, 0, 0, "")) cout << -1;
+}
