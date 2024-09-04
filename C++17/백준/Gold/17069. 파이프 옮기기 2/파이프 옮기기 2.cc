668e907d15be7f7137512ccb45c4c08c2192f62f
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2024-09-04 12:17:01 +0900

    [Gold IV] Title: 파이프 옮기기 2, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

---
+++ C++17/백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int n;
+long long d[34][34], dp[34][34][4];
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> d[i][j];
+        }
+    }
+    dp[1][2][0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 3; j <= n; j++) { 
+            if (d[i][j] == 1) continue;
+            dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
+            dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2];
+            if (d[i - 1][j] == 0 && d[i][j - 1] == 0) {
+                dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+            }
+        }
+    }
+    cout << dp[n][n][0] + dp[n][n][1] + dp[n][n][2];
+}
