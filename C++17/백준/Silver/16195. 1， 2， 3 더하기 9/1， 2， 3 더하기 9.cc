1c1e605cad3e4a7020af55afaeb37352127030e1
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2024-09-05 23:19:15 +0900

    [Silver I] Title: 1, 2, 3 더하기 9, Time: 8 ms, Memory: 5960 KB -BaekjoonHub

---
+++ C++17/백준/Silver/16195. 1， 2， 3 더하기 9/1， 2， 3 더하기 9.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+const int mod = 1000000009;
+int dp[1004][1004];
+int main() {
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    dp[0][0] = 1; 
+    for (int i = 1; i <= 1000; i++) {
+        for (int j = 1; j <= i; j++) {
+            if (i - 1 >= 0) dp[i][j] = (dp[i][j] + dp[i - 1][j - 1]) % mod;
+            if (i - 2 >= 0) dp[i][j] = (dp[i][j] + dp[i - 2][j - 1]) % mod;
+            if (i - 3 >= 0) dp[i][j] = (dp[i][j] + dp[i - 3][j - 1]) % mod;
+        }
+    }
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        long long sum=0;
+        for(int i=1;i<=m;i++) sum+=dp[n][i],sum%=mod;
+        cout<<sum<<"\n";
+    }
+}
