94e1d2cf39bf8474af598ed091fae361af0c3543
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2024-09-19 10:09:59 +0900

    [Gold V] Title: 레벨 햄버거, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Gold/16974. 레벨 햄버거/레벨 햄버거.cc
+++ C++17/백준/Gold/16974. 레벨 햄버거/레벨 햄버거.cc
@@ -1,27 +1,22 @@
 #include<iostream>
 using namespace std;
-typedef long long int ll;
-ll n, m;
-ll a[100], b[100];
-ll f(int cnt,  ll y) {
-	if (cnt == 0) {
-		if (y == 1) return 1;
-		else if (y == 0) return 0;
-	}
-	else if (y == 1) return 0;
-	else if (y <= a[cnt - 1] + 1) return f(cnt - 1, y - 1);
-	else if (y == 1 + a[cnt - 1] + 1) return b[cnt - 1] + 1;
-	else if (y <= 2*a[cnt-1]+2) return b[cnt - 1] + 1 + f(cnt - 1, y - 1 - a[cnt - 1] - 1);
-	else if (y == a[cnt]) return b[cnt];
+long long n,m;
+long long patty[54], buger[54];
+long long f(long long x,int level){
+      if(level==0){
+            if(x==1) return 1;
+            else return 0;
+      }
+      else if(x==1) return 0;
+      else if(x<=buger[level-1]+1) return f(x-1,level-1);
+      else if(x==buger[level-1]+1) return patty[level-1]+1;
+      else if(x<=2*buger[level-1]+2) return patty[level-1]+1+f(x-buger[level-1]-2,level-1);
+      else if(x==buger[level]) return patty[level];
 }
-int main() {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	cin >> n >> m;
-	a[0] = 1;
-	b[0] = 1;
-	for (int i = 1; i <= n; i++) {
-		a[i] = a[i - 1] * 2 + 3;
-		b[i] = 2 * b[i - 1] + 1;
-	}
-	cout << f(n, m);
+int main(){
+      ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+      cin>>n>>m;
+      patty[0]=1,buger[0]=1;
+      for(int i=1;i<=n;i++) patty[i]=patty[i-1]*2+1, buger[i]=buger[i-1]*2+3;
+      cout<<f(m,n);
 }
