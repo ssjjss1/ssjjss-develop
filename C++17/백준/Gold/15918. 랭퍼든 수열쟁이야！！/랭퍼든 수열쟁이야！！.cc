96f92672fc4987ba8d00ca893eb005e0da500f93
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2024-09-20 19:17:23 +0900

    [Gold V] Title: 랭퍼든 수열쟁이야!!, Time: 228 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Gold/15918. 랭퍼든 수열쟁이야！！/랭퍼든 수열쟁이야！！.cc
+++ C++17/백준/Gold/15918. 랭퍼든 수열쟁이야！！/랭퍼든 수열쟁이야！！.cc
@@ -1,53 +1,29 @@
 #include<iostream>
-
 using namespace std;
-
 int n, x, y;
-
-int a[30];
-
-int ans = 0;
-
+int a[34];
+int ans;
 void f(int cnt) {
-
-	if (cnt == n+1) {		ans += 1;
-
+	if (cnt == n + 1) {		
+	      ans += 1;
 		return;
-
 	}
-
-	if (cnt == y - x - 1) f(cnt + 1);
-
-	for (int j = 1; (j+cnt+1) < 2*n+1; j++) {
-
-		if (a[j] == 0 && a[j + cnt + 1] == 0) {
-
-			a[j] = cnt;
-
-			a[j + cnt + 1] = cnt;
-
+	if (cnt == y - x - 1) {
+	      f(cnt + 1);
+	      return;
+	}
+	for (int j = 1; (j+cnt+1) < 2*n + 1; j++) {
+		if (!a[j] && !a[j + cnt + 1]) {
+			a[j] = a[j + cnt + 1] = 1;
 			f(cnt + 1);
-
-			a[j] = 0;
-
-			a[j + cnt + 1] = 0;
-
+			a[j] = a[j + cnt + 1] = 0;
 		}
-
 	}
-
 }
-
 int main() {
-
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
+      ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 	cin >> n >> x >> y;
-
-	a[x] = y - x - 1, a[y] = y - x - 1;
-
+	a[x] = a[y] = 1;
 	f(1);
-
 	cout << ans;
-
 }
