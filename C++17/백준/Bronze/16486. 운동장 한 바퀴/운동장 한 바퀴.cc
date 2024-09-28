a46cf833018e5a41305b0e9e8b4efcf5e8b479c6
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2024-09-28 21:55:39 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+double a,b;
+int main(){
+      ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+      cin>>a>>b;
+      cout<<fixed<<setprecision(20)<<(a*2.0+2.0*b*3.141592);
+}
