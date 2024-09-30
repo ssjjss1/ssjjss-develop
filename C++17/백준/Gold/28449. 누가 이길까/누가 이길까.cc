79332b7c415771856043d66bcb1064b73886a6b5
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2024-09-30 12:44:06 +0900

    [Gold V] Title: 누가 이길까, Time: 20 ms, Memory: 3192 KB -BaekjoonHub

--- C++17/백준/Gold/28449. 누가 이길까/누가 이길까.cc
+++ C++17/백준/Gold/28449. 누가 이길까/누가 이길까.cc
@@ -2,22 +2,21 @@
 #include<algorithm>
 using namespace std;
 int n, m;
-int a[100004], b[100004];
+int a[100004], b[100004], sum[100004];
+long long a1, a2, d1;
 int main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> n >> m;
-    for (int i = 1; i <= n; i++) cin >> a[i];
-    for (int i = 1; i <= m; i++) cin >> b[i];
-    sort(a + 1, a + n + 1);
-    sort(b + 1, b + m + 1);
-    long long A = 0, B = 0, M = 0;
-    for (int i = 1; i <= m; i++) {
-        int a1 = lower_bound(a + 1, a + n + 1, b[i]) - (a + 1);
-        int b1 = n - (upper_bound(a + 1, a + n + 1, b[i]) - (a + 1)); 
-        int m1 = (upper_bound(a + 1, a + n + 1, b[i]) - lower_bound(a + 1, a + n + 1, b[i])); 
-        A += a1;
-        B += b1;
-        M += m1;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        sum[a[i]] += 1;
     }
-    cout << B << " " << A << " " << M ;
+    for (int i = 1; i <= m; i++) cin >> b[i];
+    for (int i = 1; i <= 100000; i++) sum[i] += sum[i - 1];
+    for (int i = 1; i <= m; i++) {
+        a1 += (n - sum[b[i]]);
+        a2 += sum[b[i] - 1];
+        d1 += (sum[b[i]] - sum[b[i] - 1]);
+    }
+    cout << a1 << " " << a2 << " " << d1 ;
 }
