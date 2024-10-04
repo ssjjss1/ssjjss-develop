f7e912afd36f19eeb5b73f9ea8a9d30b393311b7
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2024-10-04 12:46:56 +0900

    [Platinum V] Title: 샤워실 바닥 깔기 (Large), Time: 0 ms, Memory: 5928 KB -BaekjoonHub

--- C++17/백준/Platinum/14601. 샤워실 바닥 깔기 （Large）/샤워실 바닥 깔기 （Large）.cc
+++ C++17/백준/Platinum/14601. 샤워실 바닥 깔기 （Large）/샤워실 바닥 깔기 （Large）.cc
@@ -3,20 +3,7 @@ using namespace std;
 int n, x, y;
 int cnt;
 int a[1000][1000];
-bool visited(int x, int y, int size);
-void f(int x, int y, int size) {
-	cnt += 1;
-	if (visited(x, y, size / 2))a[x + size / 2 - 1][y + size / 2 - 1] = cnt;
-	if (visited(x, y + size / 2, size / 2))a[x + size / 2 - 1][y + size / 2] = cnt;
-	if (visited(x + size / 2, y, size / 2))a[x + size / 2][y + size / 2 - 1] = cnt;
-	if (visited(x + size / 2, y + size / 2, size / 2))a[x + size / 2 ][y + size / 2] = cnt;
-	if (size == 2) return;
-	f(x, y, size/2);
-	f(x + size/2, y, size / 2);
-	f(x, y + size/2, size / 2);
-	f(x + size/2, y + size/2, size / 2);
-}
-bool visited(int x, int y, int size) {
+int visited(int x, int y, int size) {
 	for (int i = x; i < x + size ; i++) {
 		for (int j = y; j < y + size; j++) {
 			if (a[i][j] != 0) return 0;
@@ -24,11 +11,23 @@ bool visited(int x, int y, int size) {
 	}
 	return 1;
 }
+void f(int x, int y, int size) {
+	cnt += 1;
+	if (visited(x, y, size / 2)) a[x + size / 2 - 1][y + size / 2 - 1] = cnt;
+	if (visited(x, y + size / 2, size / 2)) a[x + size / 2 - 1][y + size / 2] = cnt;
+	if (visited(x + size / 2, y, size / 2)) a[x + size / 2][y + size / 2 - 1] = cnt;
+	if (visited(x + size / 2, y + size / 2, size / 2)) a[x + size / 2 ][y + size / 2] = cnt;
+	if (size == 2) return;
+	f(x, y, size/2);
+	f(x + size/2, y, size / 2);
+	f(x, y + size/2, size / 2);
+	f(x + size/2, y + size/2, size / 2);
+}
 int main() {
-	ios::sync_with_stdio(); cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio();cin.tie(0);cout.tie(0);
 	cin >> n >> x >> y;
 	a[y][x] = -1;
-	f(1, 1 ,1<<n);
+	f(1, 1,1<<n);
 	for (int i = (1 << n); i >= 1; i--) {
 		for (int j = 1; j <= (1 << n); j++) {
 			cout << a[i][j] << " ";
