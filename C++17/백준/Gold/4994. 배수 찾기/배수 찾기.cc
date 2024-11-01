f6b7c16fab99a8ebbd14c965a7aa75ea6793e964
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2024-11-01 12:54:47 +0900

    [Gold IV] Title: 배수 찾기, Time: 120 ms, Memory: 10336 KB -BaekjoonHub

--- C++17/백준/Gold/4994. 배수 찾기/배수 찾기.cc
+++ C++17/백준/Gold/4994. 배수 찾기/배수 찾기.cc
@@ -1,27 +1,23 @@
 #include<iostream>
 #include<queue>
 using namespace std;
+typedef unsigned long long ll;
 int n;
-typedef unsigned long long int ll;
 int main(){
-  ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-  for(;;){
-    cin>>n;
-    if(n==0)return 0;
-    priority_queue<ll,vector<ll>, greater<ll>> q;
-    q.push(1);
-    while(!q.empty()){
-      ll x=q.top();
-      q.pop();
-      if(x%n==0){
-        cout<<x<<"\n";
-        break;
+      ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+      while(true){
+            cin>>n;
+            if(n==0) return 0;
+            priority_queue<ll,vector<ll>,greater<ll>> q;
+            q.push(1);
+            while(!q.empty()){
+                  ll x=q.top();
+                  q.pop();
+                  if(!(x%n)) {
+                        cout<<x<<"\n";
+                        break;
+                  }
+                  q.push(10*x+1);q.push(10*x);
+            }
       }
-      else{
-        q.push(x*10+1);
-        q.push(x*10);
-      }
-    }
-    while(!q.empty())q.pop();
-  }
 }
