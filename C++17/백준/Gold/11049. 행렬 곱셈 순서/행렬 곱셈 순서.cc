16da8ac6a0397a72435c5a5d315c4ffc36b15f06
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2024-11-01 16:45:11 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 28 ms, Memory: 3012 KB -BaekjoonHub

--- C++17/백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
+++ C++17/백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
@@ -1,26 +1,24 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<cstring>
 using namespace std;
-const int inf = 1e18 + 4;
-typedef long long ll;
-struct P {
-    int a, b;
-};
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int n;
-    cin >> n;
-    vector<P> v(n);
-    for (int i = 0; i < n; i++) cin >> v[i].a >> v[i].b;
-    vector<vector<ll>> dp(n, vector<ll>(n, 0));
-    for (int l = 2; l <= n; l++) {
-        for (int i = 0; i <= n - l; i++) {
-            int j = i + l -1;
-            dp[i][j] = inf;
-            for (int k = i; k < j; k++) {
-                ll m = dp[i][k] + dp[k + 1][j] + v[i].a * v[k].b * v[j].b;
-                dp[i][j] = min(dp[i][j], m);
+const int INF=1e9+7;
+int n,m;
+int dp[504][504];
+vector<pair<int,int>> v;
+int main(){
+      ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+      cin>>n;
+      for(int i=1,x,y;i<=n;i++) cin>>x>>y, v.push_back({x,y});
+      for(int len=2;len<=n;len++){
+            for(int i=0;i<n-len+1;i++){
+                  int j=i+len-1;
+                  dp[i][j]=INF;
+                  for(int k=i;k<j;k++){
+                        int t=dp[i][k]+dp[k+1][j]+v[i].first*v[k].second*v[j].second;
+                        dp[i][j]=min(dp[i][j], t);
+                  }
             }
-        }
-    }
-    cout << dp[0][n - 1];
+      }
+      cout<<dp[0][n-1];
 }
