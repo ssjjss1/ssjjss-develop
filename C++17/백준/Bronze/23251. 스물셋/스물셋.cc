ef7dde2b65b0338a8fcf4514b0627a27b3e55506
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2024-11-22 09:54:45 +0900

    [Bronze II] Title: 스물셋, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/23251. 스물셋/스물셋.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+long long t,k;
+int main(){
+      ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+      cin>>t;
+      while(t--){
+            cin>>k;
+            cout<<k*23<<"\n";
+      }
+}
