edfd0af08eb66823cfcc999a460f598fa57ea4e9
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2024-11-22 12:05:00 +0900

    [Gold V] Title: 나는 기말고사형 인간이야, Time: 68 ms, Memory: 7280 KB -BaekjoonHub

--- C++17/백준/Gold/23254. 나는 기말고사형 인간이야/나는 기말고사형 인간이야.cc
+++ C++17/백준/Gold/23254. 나는 기말고사형 인간이야/나는 기말고사형 인간이야.cc
@@ -3,29 +3,31 @@ using namespace std;
 int n, m, sum;
 vector<int> v, v1;
 struct compare {
-    bool operator()(pair<int, int> a, pair<int, int> b) {
-        return a.first < b.first;
-    }
+	bool operator()(pair<int, int> a, pair<int, int> b) {
+		return a.first < b.first;
+	}
 };
 
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    cin >> n >> m;
-    n *= 24;
-    for (int i = 1, x; i <= m; i++) cin >> x, v.push_back(100-x), sum += x;
-    for (int i = 1, x; i <= m; i++) cin >> x, v1.push_back(x);
-   
-    priority_queue<pair<int, int>, vector<pair<int, int>>, compare> pq; // 함수 
-    for (int i = 0; i < m; i++) pq.push({v1[i], v[i]});
-    while(!pq.empty()&&n){
-          auto [x,y]=pq.top();pq.pop();
-          if(x<=y){
-                sum+=x;
-                pq.push({x,y-x});
-                n-=1;
-          }else{
-                pq.push({y,y});
-          }
-    }
-    cout<<sum;
+	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	cin >> n >> m;
+	n *= 24;
+	for (int i = 1, x; i <= m; i++) cin >> x, v.push_back(100-x), sum += x;
+	for (int i = 1, x; i <= m; i++) cin >> x, v1.push_back(x);
+
+	priority_queue<pair<int, int>, vector<pair<int, int>>, compare> pq; // m(l
+	for (int i = 0; i < m; i++) pq.push({v1[i], v[i]});
+	while(!pq.empty()&&n) {
+		auto [x,y]=pq.top();
+		pq.pop();
+		int t=y/x;
+		int mod=y%x;
+		if(t<=n) sum+=t*x,n-=t;
+		else {
+			sum+=n*x;
+			break;
+		}
+		if(mod) pq.push({mod,mod});
+	}
+	cout<<sum;
 }
