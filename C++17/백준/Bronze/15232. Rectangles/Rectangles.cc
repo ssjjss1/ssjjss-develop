3b7d5e5dad24fd906c239e7419d4c9010530d85c
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2024-12-06 14:30:52 +0900

    [Bronze V] Title: Rectangles, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/15232. Rectangles/Rectangles.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int r,c;
+int main(){
+      ios::sync_with_stdio(0);cin.tie(0);
+      cin>>r>>c;
+      for(int i=1;i<=r;i++){
+            for(int j=1;j<=c;j++){
+                  cout<<"*";
+            }
+            cout<<"\n";
+      }
+}
