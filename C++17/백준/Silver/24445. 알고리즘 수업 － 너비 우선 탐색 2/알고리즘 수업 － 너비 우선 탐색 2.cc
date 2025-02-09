73936ccd3f067f169adc8bbdd336fc401cfce3fc
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2025-02-09 22:10:41 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 76 ms, Memory: 8840 KB -BaekjoonHub

---
+++ C++17/백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m,k,cnt;
+
+vector<int> v[100004];
+vector<int> visited(100004,0);
+
+void bfs(int num){
+      queue<int> q;
+      visited[num]=++cnt;
+      q.push(num);
+      while(!q.empty()){
+            int x=q.front();
+            q.pop();
+            for(auto i:v[x]){
+                  if(!visited[i]){
+                        visited[i]=++cnt;
+                        q.push(i);
+                  }
+            }
+      }
+}
+
+int main(){
+      ios_base::sync_with_stdio(0); cin.tie(0);
+      cin>>n>>m>>k;
+      
+      for(int i=1;i<=m;i++){
+            int x,y;
+            cin>>x>>y;
+            v[x].push_back(y);
+            v[y].push_back(x);
+      }
+      
+      for(int i=1;i<=n;i++) sort(v[i].begin(),v[i].end(),greater<>());
+      
+      bfs(k);
+      
+      for(int i=1;i<=n;i++) cout<<visited[i]<<"\n";
+}
