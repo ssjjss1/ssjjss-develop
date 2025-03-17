4b227445dc2223b73c8d4a9c3252e8c8416715e3
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2025-03-17 22:35:09 +0900

    [Gold V] Title: 2배 또는 0.5배, Time: 168 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- C++17/백준/Gold/27468. 2배 또는 0．5배/2배 또는 0．5배.cc
+++ C++17/백준/Gold/27468. 2배 또는 0．5배/2배 또는 0．5배.cc
@@ -1,40 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n;
-int dx[]={1,1,2,-1};
-int main(){
-      ios_base::sync_with_stdio(0);
-      cin.tie(0);
-      
-      cin>>n;
-      
-      if(n==3) {
-            cout<<"YES\n"<<1<<" "<<3<<" "<<2;
-      } else{
-            if(n%4==3) cout<<"NO";
-            else {
-                  cout<<"YES\n";
-                  int idx=n/4+(n%4?1:0);
-                  
-                  for(int i=0;i<idx;i++){
-                        
-                        int num=i*4;
-                        
-                        if(i!=idx-1){
-                              
-                              for(int j=0;j<4;j++){
-                                    num+=dx[j];
-                                    cout<<num<<" ";
-                              }
-                        } else{
-                              int lim=(n%4==0?4:n%4);
-                              
-                              for(int j=0;j<lim;j++){
-                                    num+=dx[j];
-                                    cout<<num<<" ";
-                              }
-                        }
-                  }
-            }
-      }
+int dx[]= {0,1,-1,0}, dy[]={0,0,1,-1};
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin>>n;
+
+	cout<<"YES\n";
+	if(n%2==0) {
+		for(int i=1; i<=n; i++) {
+			cout<<i+(dy[(i-1)%4])<<" ";
+		}
+	} else {
+
+		int lim=n/4+(n%4?1:0);
+		for(int i=0; i<lim; i++) {
+
+			int num=i*4+1;
+			if(i!=lim-1) {
+				for(int j=0; j<4; j++) {
+					cout<<num+dx[j]+j<<" ";
+				}
+			} else{
+			      
+			      int inf=n%4;
+			      for(int j=0; j<inf; j++) {
+					cout<<num+dx[j]+j<<" ";
+				}
+			}
+
+
+		}
+	}
 }
