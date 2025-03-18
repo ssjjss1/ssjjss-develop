3bec0832a5850968ab81e230f4b3cbae9588f5f3
ssjjss1 <163983167+ssjjss1@users.noreply.github.com>
2025-03-18 14:13:02 +0900

    [Platinum V] Title: 수열과 쿼리 HY, Time: 456 ms, Memory: 5540 KB -BaekjoonHub

--- C++17/백준/Platinum/32870. 수열과 쿼리 HY/수열과 쿼리 HY.cc
+++ C++17/백준/Platinum/32870. 수열과 쿼리 HY/수열과 쿼리 HY.cc
@@ -7,47 +7,46 @@ vector<int> A;
 pair<int, int> ans[300004];
 
 void Init() {
-    for (int i = 1; i <= 300000; i++) {
-        ans[i] = {INF, -INF};
-    }
+	for (int i = 1; i <= 300000; i++) {
+		ans[i] = {INF, -INF};
+	}
 }
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
 
-    cin >> n >> m;
-    A.resize(n);
+	cin >> n >> m;
+	A.resize(n);
 
-    Init();
+	Init();
 
-    for (int i = 0; i < n; i++) {
-        cin >> A[i];
-    }
+	for (int i = 0; i < n; i++) {
+		cin >> A[i];
+	}
 
-    sort(A.begin(), A.end());
+	sort(A.begin(), A.end());
 
-    ans[1] = {0, 0};
+	ans[1] = {0, 0};
 
-    for (int i = 2; i <= 300000; i++) {
-        for (int j = 0; j <= 600000; j += i) {
-            auto it = lower_bound(A.begin(), A.end(), j);
+	while (m--) {
+		int x;
+		cin >> x;
+		if(ans[x].first==INF) {
+			for (int j = 0; j <= 600000; j += x) {
+				auto it = lower_bound(A.begin(), A.end(), j);
 
-            if (it != A.end()) {
-                ans[i].first = min(ans[i].first, (*it % i));
-                ans[i].second = max(ans[i].second, (*it % i));
-            }
-            if (it != A.begin()) {
-                it--;
-                ans[i].first = min(ans[i].first, (*it % i));
-                ans[i].second = max(ans[i].second, (*it % i));
-            }
-        }
-    }
-
-    while (m--) {
-        int x;
-        cin >> x;
-        cout << ans[x].first << " " << ans[x].second << "\n";
-    }
+				if (it != A.end()) {
+					ans[x].first = min(ans[x].first, (*it % x));
+					ans[x].second = max(ans[x].second, (*it % x));
+				}
+				if (it != A.begin()) {
+					it--;
+					ans[x].first = min(ans[x].first, (*it % x));
+					ans[x].second = max(ans[x].second, (*it % x));
+				}
+			}
+		}
+		cout << ans[x].first << " " << ans[x].second << "\n";
+	}
 }
